a4de9e4ea37d5fcb95aa072bc9b38acac392237f
bmagario <brian.magario@gmail.com>
2023-10-31 13:15:39 -0300

    add postamn collection

--- sum-calculator-service/src/main/java/com/microservices/challenge/sumcalculatorservice/controller/SumCalculatorController.java
+++ sum-calculator-service/src/main/java/com/microservices/challenge/sumcalculatorservice/controller/SumCalculatorController.java
@@ -1,11 +1,10 @@
 package com.microservices.challenge.sumcalculatorservice.controller;
 
-import com.microservices.challenge.sumcalculatorservice.dto.CalculationRequestDTO;
 import com.microservices.challenge.sumcalculatorservice.service.SumCalculatorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +18,10 @@ public class SumCalculatorController {
     }
 
     @GetMapping("/calculate")
-    public ResponseEntity<Double> calculate(@RequestBody CalculationRequestDTO requestDTO) {
+    public ResponseEntity<Double> calculate(@RequestParam(name = "num1") Double num1,
+                                            @RequestParam(name = "num2") Double num2) {
         Double result =
-                sumCalculatorService.performCalculation(requestDTO.getNum1(), requestDTO.getNum2());
+                sumCalculatorService.performCalculation(num1, num2);
         return ResponseEntity.ok(result);
     }
 }
